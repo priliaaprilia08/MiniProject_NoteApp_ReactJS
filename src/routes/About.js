0f606deba6b8035756806159228d585d97163aeb
priliaaprilia08 <apriliapriliarahmawatia@gmail.com>
2022-11-11 14:02:09 +0700

    tugas miniproject

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Hero from "../components/Hero";
+// import 'bootstrap/dist/css/bootstrap.css';
 import "../css/About.css";
 
 const About = () => {
@@ -15,8 +16,7 @@ const About = () => {
               Tips & Trik Menyusun Notes<span>App</span>
             </h2>
             <p>
-              Includes every feature we offer plus unlimited projects and
-              unlimited users.
+              Cobalah terlebih dauhulu, sebelum mengatakan tidak pada semua yang dianggap sulit.
             </p>
           </div>
           <div className="btn-div">
@@ -31,70 +31,11 @@ const About = () => {
         <div className="bottom-content">
           <div className="card">
             <p>
-              <span>Everything you need</span>
+              <span>Upayakan Mencantumkan Durasi Pengerjaan</span>
             </p>
             {/* <h3>All-in-one platform</h3> */}
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-              soluta, temporibus ea est ipsum vero recusandae blanditiis ut
-              doloribus excepturi!
-            </p>
-            {/* <button
-              type="button"
-              class="btn btn-primary"
-              data-bs-toggle="modal"
-              data-bs-target="#staticBackdrop"
-            >
-              Read Modul
-            </button> */}
-
-            {/* <div
-              class="modal fade"
-              id="staticBackdrop"
-              data-bs-backdrop="static"
-              data-bs-keyboard="false"
-              tabindex="-1"
-              aria-labelledby="staticBackdropLabel"
-              aria-hidden="true"
-            >
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">
-                      Modal title
-                    </h1>
-                    <button
-                      type="button"
-                      class="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
-                  <div class="modal-body">...</div>
-                  <div class="modal-footer">
-                    <button
-                      type="button"
-                      class="btn btn-secondary"
-                      data-bs-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    <button type="button" class="btn btn-primary">
-                      Understood
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div> */}
-          </div>
-          <div className="card">
-            <p>
-              <span>Everything you need</span>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-              soluta, temporibus ea est ipsum vero recusandae blanditiis ut
-              doloribus excepturi!
+             Durasi akan menjadi kunci penting kamu dalam membuat jadwal dan kegiatan lebih teratur. Dengan durasi setiap aktivitas akan lebih mudah untuk dipetakan dengan metode filter, sehingga jangan lupa cantumkan durasi ya!
             </p>
             {/* <button
               type="button"
@@ -146,12 +87,10 @@ const About = () => {
           </div>
           <div className="card">
             <p>
-              <span>Everything you need</span>
+              <span>CATAT SEMUA AKTIVITAS APAPUN</span>
             </p>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-              soluta, temporibus ea est ipsum vero recusandae blanditiis ut
-              doloribus excepturi!
+              Dalam melakukan banyak kegiatan kita sering kali lupa akan kegitan yang sudah kita lakukan, sehingaa kita perlu mempunyai cara untuk mudah mengingat dan melist kegiatan apa saja yang sudah kita lakukan. So, jangan lupa catatan kegiatan kamu!
             </p>
             {/* <button
               type="button"
@@ -160,8 +99,63 @@ const About = () => {
               data-bs-target="#staticBackdrop"
             >
               Read Modul
-            </button> */}
-            {/* <div
+            </button>
+
+            <div
+              class="modal fade"
+              id="staticBackdrop"
+              data-bs-backdrop="static"
+              data-bs-keyboard="false"
+              tabindex="-1"
+              aria-labelledby="staticBackdropLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                      Modal title
+                    </h1>
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body">...</div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                    <button type="button" class="btn btn-primary">
+                      Understood
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div> */}
+          </div>
+          <div className="card">
+            <p>
+              <span>Atur Prioritas Pekerjaan</span>
+            </p>
+            <p>
+              Apabila kita tau bagaimana mengatur prioritas pekerjaan. Maka, akan lebih mudah kita mengatur prioritas tersebut dengan menceklis list kegiatan/jadwal yang bersifat penting dan tidak genting, penting dan genting, tidak penting dan genting, ataupun tidak penting dan tidak genting!
+            </p>
+            {/* <button
+              type="button"
+              class="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+            >
+              Read Modul
+            </button> 
+            <div
               class="modal fade"
               id="staticBackdrop"
               data-bs-backdrop="static"
